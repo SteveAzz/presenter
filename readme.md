cd2609cd357257accdca3f84b2483203e3abff90
SteveAzz <steveazz@outlook.com>
2015-05-29 09:57:32 +0000

    Aded Presenter Parent class

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Presenter
+A presenter class that helps you decouple the present logic from your controller and from your view.
--- src/SteveAzz/Presenters/Presenter.php
+++ src/SteveAzz/Presenters/Presenter.php
@@ -1,7 +1,34 @@
 <?php namespace SteveAzz\Presenters;
 
 
-class Presenter
+abstract class Presenter
 {
 
+    /**
+     * @var
+     */
+    protected $model;
+
+    /**
+     * @param $model
+     */
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Magic method to call any method name.
+     *
+     * @param $property
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        if (method_exists($this, $property)) {
+            return $this->{$property}();
+        }
+
+        return $this->model->{$property};
+    }
 }
